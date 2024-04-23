f43882e07056c28e4889f20f9200f54199c01075
yinke <yinke@eastmoney.com>
2024-04-23 14:49:04 +0800

    提前5天放票

--- utils.py
+++ utils.py
@@ -2,9 +2,11 @@ import time
 import datetime
 import logging
 logging.basicConfig(level=logging.INFO)
-def time_ahead_sale(target_date):
+def time_ahead_sale(sail_date):
     """计算当前时间到目标日期6点半的秒数，以便休眠到该时间点"""
     now = datetime.datetime.now()
-    start_date=datetime.datetime.strptime(target_date, '%Y-%m-%d').date()
-    start_datetime = datetime.datetime.combine(start_date, datetime.time(6, 50))
+    sail_date=datetime.datetime.strptime(sail_date, '%Y-%m-%d').date()
+    # 提前5天放票
+    start_qiangpiao_date=sail_date - datetime.timedelta(days=4)
+    start_datetime = datetime.datetime.combine(start_qiangpiao_date, datetime.time(6, 50))
     return (start_datetime - now).total_seconds()
