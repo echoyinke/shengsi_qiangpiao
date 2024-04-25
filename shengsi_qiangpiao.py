dd4cd9668ff914bb7a43c1b5727695dac3dec568
yinke <yinke@eastmoney.com>
2024-04-25 10:30:23 +0800

    add more config

--- shengsi_qiangpiao.py
+++ shengsi_qiangpiao.py
@@ -9,15 +9,33 @@ yamlPath = os.path.join(fileNamePath, 'config_local.yaml')
 cf = yaml.load(open(yamlPath, 'r', encoding='utf-8').read(), Loader=yaml.FullLoader)
 startPortNo=cf['PortNo']['nanpu']
 endPortNo=cf['PortNo']['gouqi']
-sail_date="2024-05-01"
-
+sail_date="2024-05-02"
+seat_need=3
+seat_class_priority=["上舱","商务舱","中舱", "下舱", "三等舱", "普舱"]
+depart_as_later_as_posible=False
+need_order=True
 
 logging.info(f"Starting shengsi qiangpiao script{startPortNo} 到 {endPortNo} on {sail_date} ...")
 
 n=0
+
+
+def pick_sail(data):
+    available_seat_info = ""
+    if depart_as_later_as_posible:
+        data = sorted(data, key=lambda x: x['sailTime'], reverse=True)
+    for info in data:
+        seatClasses = sorted(info['seatClasses'], key=lambda x: seat_class_priority.index(x['className']) if x['className'] in seat_class_priority else len(seat_class_priority))
+        for seatClass in seatClasses:
+            if seatClass['pubCurrentCount'] >=seat_need:
+                available_seat_info += f"{seatClass['className']} 空余 {seatClass['pubCurrentCount']};"
+                content = f"{info['startPortName']} 到 {info['endPortName']} {info['sailDate']}  {info['sailTime']} " + available_seat_info
+                send_dingtalk(content)
+        return info
+
 while True:
     need_wait_time = get_next_check_time()
-    if need_wait_time == 0:
+    if True or need_wait_time == 0:
         n+=1
         logging.info(f"Trying qiangpiao {n} times ...")
         token=get_token()
@@ -29,16 +47,10 @@ while True:
             time.sleep(1)
             continue
         # core logic
-        info=response['data'][0]
-        available_seat_info=""
-        seatClasses = info['seatClasses']
-        for seatClass in seatClasses:
-            if seatClass['pubCurrentCount'] > 0:
-                available_seat_info += f"{seatClass['className']} 空余 {seatClass['pubCurrentCount']};"
-        if available_seat_info !='':
-            content = f"{info['startPortName']} 到 {info['endPortName']} {info['sailDate']}  {info['sailTime']} " + available_seat_info
-            send_dingtalk(content)
-            order_id=save_seats(sail_date=sail_date, line_no=info['lineNo'], token=token)
+        data=response['data']
+        sail_info=pick_sail(data)
+        if need_order:
+            order_id=save_seats(sail_date=sail_date, line_no=sail_info['lineNo'], token=token)
             if order_id is not None:
                 send_dingtalk("抢票成功！！！请尽快前往支付")
                 break
