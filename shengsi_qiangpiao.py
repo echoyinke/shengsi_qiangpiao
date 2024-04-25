4bd879a1eef53597b61c84ee1ed4f144cc57beca
yinke <yinke@eastmoney.com>
2024-04-25 11:02:00 +0800

    modify

--- shengsi_qiangpiao.py
+++ shengsi_qiangpiao.py
@@ -7,32 +7,33 @@ from utils import *
 fileNamePath = os.path.split(os.path.realpath(__file__))[0]
 yamlPath = os.path.join(fileNamePath, 'config_local.yaml')
 cf = yaml.load(open(yamlPath, 'r', encoding='utf-8').read(), Loader=yaml.FullLoader)
-startPortNo=cf['PortNo']['nanpu']
-endPortNo=cf['PortNo']['gouqi']
-sail_date="2024-05-02"
-seat_need=3
+startPortNo=cf['PortNo']['gouqi']
+endPortNo=cf['PortNo']['nanpu']
+sail_date="2024-05-04"
+seats_need=3
 seat_class_priority=["上舱","商务舱","中舱", "下舱", "三等舱", "普舱"]
 depart_as_later_as_posible=False
-need_order=True
-
+need_order=False
 logging.info(f"Starting shengsi qiangpiao script{startPortNo} 到 {endPortNo} on {sail_date} ...")
 
 n=0
 
 
-def pick_sail(data):
+def pick_sail(sails):
     available_seat_info = ""
     if depart_as_later_as_posible:
-        data = sorted(data, key=lambda x: x['sailTime'], reverse=True)
-    for info in data:
-        seatClasses = sorted(info['seatClasses'], key=lambda x: seat_class_priority.index(x['className']) if x['className'] in seat_class_priority else len(seat_class_priority))
+        data = sorted(sails, key=lambda x: x['sailTime'], reverse=True)
+    for sail in sails:
+        seatClasses = sorted(sail['seatClasses'], key=lambda x: seat_class_priority.index(x['className']) if x['className'] in seat_class_priority else len(seat_class_priority))
         for seatClass in seatClasses:
-            if seatClass['pubCurrentCount'] >=seat_need:
+            if seatClass['pubCurrentCount'] >= seats_need:
                 available_seat_info += f"{seatClass['className']} 空余 {seatClass['pubCurrentCount']};"
-                content = f"{info['startPortName']} 到 {info['endPortName']} {info['sailDate']}  {info['sailTime']} " + available_seat_info
+                content = f"{sail['startPortName']} 到 {sail['endPortName']} {sail['sailDate']}  {sail['sailTime']} " + available_seat_info
                 send_dingtalk(content)
-        return info
-
+                sail['seatClasses'][:] = []
+                sail['seatClasses'].append(seatClass)
+                return sail
+    return None
 while True:
     need_wait_time = get_next_check_time()
     if True or need_wait_time == 0:
@@ -47,10 +48,10 @@ while True:
             time.sleep(1)
             continue
         # core logic
-        data=response['data']
-        sail_info=pick_sail(data)
-        if need_order:
-            order_id=save_seats(sail_date=sail_date, line_no=sail_info['lineNo'], token=token)
+        sails=response['data']
+        sail=pick_sail(sails)
+        if need_order and sail is not None:
+            order_id=save_seats(sail_date=sail_date, line_no=sail['lineNo'], token=token)
             if order_id is not None:
                 send_dingtalk("抢票成功！！！请尽快前往支付")
                 break
